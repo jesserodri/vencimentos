065cb0320ff05bd8cac9ecc0843776767e1809bb
Jessé Rodrigues da Silva <70242043+jesserodri@users.noreply.github.com>
2026-07-09 21:41:58 -0300

    Correções nos gastos e aba de investimentos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vencimentos-v8';
+const CACHE = 'vencimentos-v11';
 const ASSETS = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
